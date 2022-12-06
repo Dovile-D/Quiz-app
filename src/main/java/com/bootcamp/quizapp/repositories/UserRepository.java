d2c01cca7439c560f8ad4db2f34de4b68bbd9b34
Dovile <dovile.daulenskiene@gmail.com>
2022-12-06 18:28:59 +0200

    add User, Category, Level & Statistic repositories

--- src/main/java/com/bootcamp/quizapp/repositories/UserRepository.java
+++ src/main/java/com/bootcamp/quizapp/repositories/UserRepository.java
@@ -1,4 +1,7 @@
 package com.bootcamp.quizapp.repositories;
 
-public interface UserRepository {
+import com.bootcamp.quizapp.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
 }
