06c58bed66b314f1071f4dab9a6848323b8d05bb
Dovile <dovile.daulenskiene@gmail.com>
2022-12-13 22:49:08 +0200

    remove unused files

--- src/main/resources/sql/levels.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO difficulty_level (level_name) VALUES ('easy');
-INSERT INTO difficulty_level (level_name) VALUES ('medium');
-INSERT INTO difficulty_level (level_name) VALUES ('hard');
-
