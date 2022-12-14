6b4d140861cc74156cfbee23b59ebd19f8149c72
Vaidas V <114639626+VaidasVa@users.noreply.github.com>
2022-12-14 17:18:22 +0200

    Revert "Adjustments to game logics, calculations and data flows"

--- src/main/resources/static/singleGameStatistics.js
+++ src/main/resources/static/singleGameStatistics.js
@@ -1,20 +1,9 @@
-const queryURLString = window.location.search;
-const urlParameters = new URLSearchParams(queryURLString);
-var score = urlParameters.get('score');
-var totalQ = urlParameters.get('totalQ');
-var user = urlParameters.get('user');
-console.log(" Score " + score);
-console.log(" Total Questions " + totalQ);
-console.log(" User " + user);
+document.getElementById("singleScore").textContent = sessionStorage.getItem("gameScore") + " / " + sessionStorage.getItem("totalAnsweredQuestions");
 
-document.getElementById("resultInfo").textContent = score + " / " + totalQ;
+document.getElementById("closeSingleStatistics", resetParams);
 
-document.getElementById("gameExitButton").addEventListener("click", singleGameExit);
-
-function singleGameExit(e){
+function resetParams(e) {
     e.preventDefault();
-    if (!user){
-        window.location.replace("http://localhost:8080");
-    }
-    else {window.location.replace("http://localhost:8080/userOptions?user="+user)}
+    sessionStorage.setItem("gameScore", null);
+    sessionStorage.setItem("totalAnsweredQuestions", null);
 }
