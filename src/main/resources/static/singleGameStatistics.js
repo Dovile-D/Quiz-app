c75ecce01ffbb905c2391312b3dd5e75c9cb8831
Vaidas V <valikonis.vaidas@gmail.com>
2022-12-15 13:32:38 +0200

    If registered user, UserName showed in single game results window

--- src/main/resources/static/singleGameStatistics.js
+++ src/main/resources/static/singleGameStatistics.js
@@ -3,9 +3,11 @@ const urlParameters = new URLSearchParams(queryURLString);
 var score = urlParameters.get('score');
 var totalQ = urlParameters.get('totalQ');
 var user = urlParameters.get('user');
-//console.log(" Score " + score);
-//console.log(" Total Questions " + totalQ);
-//console.log(" User " + user);
+
+if (sessionStorage.getItem("userID") != null) {
+document.getElementById("card-title").textContent = "Amazing, " +sessionStorage.getItem("userName")+ "!"
+}
+else {}
 
 //document.getElementById("resultInfo").textContent = score;
 let result = Math.round(sessionStorage.getItem("gameScore") / 2);
